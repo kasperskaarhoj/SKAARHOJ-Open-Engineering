d17abd02e30d3e6089ecd3c17c719bf1ca18124d
Dennis Fricke <github@dennis-fricke.de>
2016-08-08 21:00:52 +0200

    Update ATEMbase.h

--- ArduinoLibs/ATEMbase/ATEMbase.h
+++ ArduinoLibs/ATEMbase/ATEMbase.h
@@ -50,7 +50,7 @@ public:
     virtual void begin(uint16_t);
     virtual int beginPacket(IPAddress ip, uint16_t port);
     virtual int endPacket();
-    virtual size_t write(uint16_t);
+    virtual size_t write(const uint8_t *buffer, size_t size);
     virtual int parsePacket();
     virtual int available();
     virtual int read(unsigned char* buffer, size_t len);
