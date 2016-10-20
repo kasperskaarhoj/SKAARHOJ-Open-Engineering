0d82ffde1e19432fa4263a8608f74ac91fce1fc5
Filip Sandborg-Olsen <filipsandborg@gmail.com>
2016-10-20 15:15:13 +0200

    Updates

--- ArduinoLibs/ClientBMDHyperdeckStudio/ClientBMDHyperdeckStudio.cpp
+++ ArduinoLibs/ClientBMDHyperdeckStudio/ClientBMDHyperdeckStudio.cpp
@@ -631,7 +631,9 @@ void ClientBMDHyperdeckStudio::askForClips(bool askForClips) { _askForClips = as
  GETTING VALUES
  ********************/
 
+// Use getPlaybackSlotId() instead!
 //uint8_t ClientBMDHyperdeckStudio::getCurrentSlotId() { return _Hyperdeck_currentSlotId; }
+
 uint8_t ClientBMDHyperdeckStudio::getSlotStatus(uint8_t slotId) { return _Hyperdeck_slotStatus[slotId - 1]; }
 unsigned long ClientBMDHyperdeckStudio::getSlotRecordingTime(uint8_t slotId) { return _Hyperdeck_slotRecordingTime[slotId - 1]; }
 uint8_t ClientBMDHyperdeckStudio::getTransportStatus() { return _Hyperdeck_transportStatus; }
