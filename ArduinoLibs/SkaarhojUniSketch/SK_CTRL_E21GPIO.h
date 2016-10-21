09dd6a7eb69108582e832dedbc43ab55c6a7e6e2
Kasper Skaarhoj <kasperskaarhoj@gmail.com>
2016-10-22 00:28:16 +0200

    options support

--- ArduinoLibs/SkaarhojUniSketch/SK_CTRL_E21GPIO.h
+++ ArduinoLibs/SkaarhojUniSketch/SK_CTRL_E21GPIO.h
@@ -8,8 +8,16 @@ void HWcfgDisplay() { Serial << F("SK_MODEL=SK_E21GPIO\n"); }
  */
 uint8_t HWsetupL() {
 
+#if SK_E21GPIO_OPTION_GPIO2
+  Serial << F("Option: GPIO2\n");
   Serial << F("Init GPIO 2 board\n");
   GPIOboard2.begin(1);
+#else
+  for (uint8_t a = 16; a < 16 + 16; a++) {
+    if (a < SK_HWCCOUNT) // Just making sure...
+      HWdis[a] = 1;	// Removes from web interface
+  }
+#endif
 
   return 0;
 }
@@ -17,7 +25,11 @@ uint8_t HWsetupL() {
 /**
  * Hardware test
  */
-void HWtestL() { GPIOboard2.setOutputAll(GPIOboard2.inputIsActiveAll() ^ ((millis() + 500) >> 12)); }
+void HWtestL() {
+#if SK_E21GPIO_OPTION_GPIO2
+  GPIOboard2.setOutputAll(GPIOboard2.inputIsActiveAll() ^ ((millis() + 500) >> 12));
+#endif
+}
 
 /**
  * Hardware runloop
@@ -29,17 +41,17 @@ void HWrunLoop() {
   HWrunLoop_GPIO(GPIOboard, 1, gpioStates);
 #endif
 
+#if SK_E21GPIO_OPTION_GPIO2
   static bool gpioStates2[] = {false, false, false, false, false, false, false, false};
   HWrunLoop_GPIO(GPIOboard2, 17, gpioStates2);
+#endif
 }
 
 uint8_t HWnumOfAnalogComponents() { return 0; }
 
-int16_t HWAnalogComponentValue(uint8_t num) {
-	return 0;
-}
+int16_t HWAnalogComponentValue(uint8_t num) { return 0; }
 
 uint16_t *HWMinCalibrationValues(uint8_t num) {
-  static uint16_t values[3] = {0,0,0};
+  static uint16_t values[3] = {0, 0, 0};
   return values;
 }
